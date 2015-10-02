3f0f7309b4b3938a4b6c5a625bf013241b4736e4
ayarborough <personal.ajy@gmail.com>
2015-10-02 13:07:45 +0100

    Started putting together controls for an advanced filter box

---
+++ EmployeeInventory/XAML/UserControls/FilterBox.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace ES.InventoryRegister.XAML.UserControls
+{
+    /// <summary>
+    /// Interaction logic for FilterBox.xaml
+    /// </summary>
+    public partial class FilterBox : UserControl
+    {
+        public FilterBox()
+        {
+            InitializeComponent();
+        }
+    }
+}
